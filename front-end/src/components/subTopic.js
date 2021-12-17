4d593cf5219c13f5d4cd0ee184fe7e5a9aa2d433
sepali <wije.sepali@gmail.com>
2021-12-17 22:50:09 +0530

    front end  codeadded

---
+++ front-end/src/components/subTopic.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class subTopic extends React.Component {
+    
+    render() {
+      return (
+        <div className="sticky-top">
+          <h1>Suggestions</h1>
+        </div>
+      );
+    }
+  }
+
+export default subTopic;
---
+++ front-end/src/data/word.js
@@ -0,0 +1,5 @@
+export const word = [
+    {
+        "word" : "කළමනාකරු"
+    }
+]
