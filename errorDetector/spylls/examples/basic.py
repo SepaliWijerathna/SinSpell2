005adb195c994eebdd9dac8e31f1f7f4430d1533
Hasini <hasinir.17@cse.mrt.ac.lk>
2022-01-29 11:27:42 +0530

    Removed unnecessary files and updated.

--- errorDetector/spylls/examples/basic.py
+++ errorDetector/spylls/examples/basic.py
@@ -29,36 +29,3 @@ def detector_fun(word):
             print(incorrect_word_list)
     
     return incorrect_word_list
-    # print('Output: '+result+'\n')
-    # print("Suggestions :")
-    # for incorrect_word in incorrect_word_list:
-    #     print("incorrect")
-    #     print([*dictionary.suggest(incorrect_word)])
-
-#........................
-
-# input_words = input("Enter your words: ")
-# word_list = input_words.split(" ")
-# correct_word_list = []
-# incorrect_word_list = []
-
-# for word in word_list:
-#     if dictionary.lookup(word):
-#         correct_word_list.append(word)
-#     else:
-#         incorrect_word_list.append(word)
-
-# print("Correct_word_list :")
-# print(correct_word_list)
-# print("Incorrect_word_list :")
-# print(incorrect_word_list)
-
-# print("Suggestions :")
-# for incorrect_word in incorrect_word_list:
-#     print([*dictionary.suggest(incorrect_word)])
-
-#........................
-
-# print(dictionary.lookup('මල්'))
-# print(dictionary.lookup('ළමයා'))
-# print([*dictionary.suggest('කලගුන')])
