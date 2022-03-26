1b720f83ba3eddf9748ad91c00b77f3a4939470f
Hasini Weerasooriya <41505060+HasiniRangana@users.noreply.github.com>
2022-03-26 14:14:15 +0530

    Add full stop identification to backend.

--- API/spylls/examples/basic.py
+++ API/spylls/examples/basic.py
@@ -5,30 +5,33 @@ from spylls.hunspell import Dictionary
 
 dictionary = Dictionary.from_files(str(path))
 
-def detector_fun(word):
-    #print(word)
-    file_content = word
+def detector_fun(file_content):    
+    # file_content = word
    
-    characters = [".",",","%","!","@","&"]
-    test_string = ''.join(i for i in file_content if not i in characters)
+    # characters = [".",",","%","!","@","&"]
+    # test_string = ''.join(i for i in file_content if not i in characters)
 
 
     word_list = file_content.split(" ")
-    #print(word_list)
     incorrect_word_list = []
     correct_word_list = []
-    result = file_content
+    # result = file_content
+    check_list = []
+    print(word_list)
+    for item in word_list:
+        if item!="":
+            if item[-1] == ".":
+                check_list.append(item[:-1])
+                check_list.append(item[-1:])
+            else: 
+                check_list.append(item)
+    print(check_list)
 
-    for word in word_list:
-        if dictionary.lookup(word):
-            # print("correct")
+    for word in check_list:
+        if dictionary.lookup(word):            
             correct_word_list.append(word)
-        else: 
-            # print("Incorrect")   
-            # marked = "\u0332".join(word+ " ")
-            # result = result.replace(word,marked)
+        else:            
             incorrect_word_list.append(word)
-            # print(incorrect_word_list)
-    
+                
     return incorrect_word_list,correct_word_list,word_list
    
